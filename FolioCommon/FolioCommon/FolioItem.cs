fee6a9d96c8a7e169e6e9413bc4ffb08699ba3ff
Bezimus <andrew@aberry.id.au>
2025-01-11 11:25:53 +0800

    Add project files.

---
+++ FolioCommon/FolioCommon/FolioItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FolioCommon
+
+    internal enum StatusType {InDevelopment,Released }
+
+{
+    internal class FolioItem
+    {
+        public int ID {  get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Type { get; set; }
+        public StatusType Status { get; set; }
+
+    public FolioItem(int id, string name, string desc, string type, StatusType status)
+    {
+        this.ID = id;
+        this.Name = name;
+        this.Description = desc;
+        this.Type = type;
+        this.Status = status;
+
+    }
+
+    }
+}
